b46c2d863cce95d5c94d112107a1801427c223c7
Kingsley Ijomah <git-example@codehance>
2022-08-25 10:38:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from './components/button/button';
+
+function App() {
+  return (
+    <>
+      <Button label="Click me now!" />
+    </>
+  );
+}
+
+export default App;
